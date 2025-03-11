932038a89d761c868a7c9d01b6a6efba6f941c43
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-03-12 00:47:26 +0600

    working on video section

--- js/categories.js
+++ js/categories.js
@@ -4,6 +4,12 @@ function loadCategories(){
     .then((data) => displayCategories(data.categories));
 }
 
+function loadVideos(){
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.videos));
+}
+
 function displayCategories(categories){
     // get the container
     const categoryContainer = document.getElementById('category-cntainer');
@@ -21,4 +27,34 @@ function displayCategories(categories){
     
 }
 
+const displayVideos = (videos) => {
+    const videoConatiner = document.getElementById('video-container')
+
+    videos.forEach((video) => {
+        console.log(video);
+
+        const videoCard = document.createElement('div');
+        videoCard.innerHTML = `
+            <div class="card bg-base-100 shadow-sm">
+            <figure>
+                <img
+                src="${video.thumbnail}"
+                alt="videos" />
+            </figure>
+            <div class="card-body">
+                <h2 class="card-title">${video.title}</h2>
+                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+            </div>
+        `;
+
+        videoConatiner.appendChild(videoCard);
+    })
+
+}
+
 loadCategories();
+loadVideos();
